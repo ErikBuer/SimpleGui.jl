b4562d6f5e350fc84abccaaa76120a611bd62f2f
Erik Buer <buer94@gmail.com>
2025-05-10 20:52:32 +0200

    refactor: update window handling to use window_state for rendering
    Signed-off-by: Erik Buer <buer94@gmail.com>

--- test/draw_png_simplified.jl
+++ test/draw_png_simplified.jl
@@ -7,9 +7,7 @@ using GeometryBasics
 using FileIO, IndirectArrays, ColorTypes, OffsetArrays
 
 
-window = initialize_window("Simple GUI Example", (1920, 1080))
-GLFW.MakeContextCurrent(window)
-GLA.set_context!(window)
+window_state = initialize_window("Simple GUI Example", 1920, 1080)
 
 file_path = "test/images/logo.png"
 
@@ -20,21 +18,21 @@ tex = SimpleGui.load_texture(file_path)  # Replace with the path to your image f
 glClearColor(0, 0, 0, 1)
 
 # Main rendering loop
-while !GLFW.WindowShouldClose(window)
+while !GLFW.WindowShouldClose(window_state.handle)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
     # Draw the image at position (0.0, 0.0)
-    SimpleGui.draw_image(tex, 0.0, 0.0, scale=0.2f0)
+    SimpleGui.draw_image(tex, 0.0, 0.0, window_state.projection_matrix, scale=0.2f0)
 
     # Swap buffers and poll events
-    GLFW.SwapBuffers(window)
+    GLFW.SwapBuffers(window_state.handle)
     GLFW.PollEvents()
 
     # Exit on ESC key press
-    if GLFW.GetKey(window, GLFW.KEY_ESCAPE) == GLFW.PRESS
-        GLFW.SetWindowShouldClose(window, true)
+    if GLFW.GetKey(window_state.handle, GLFW.KEY_ESCAPE) == GLFW.PRESS
+        GLFW.SetWindowShouldClose(window_state.handle, true)
     end
 end
 
-# Destroy the window
-GLFW.DestroyWindow(window)
+# Destroy the window_state
+GLFW.DestroyWindow(window_state.handle)
