8c672b596b6c516befcb5ef57d2aa70bfef23a26
Erik Buer <buer94@gmail.com>
2025-06-09 15:11:55 +0200

    bundle package with a default font
    Signed-off-by: Erik Buer <buer94@gmail.com>

--- src/text/text_style.jl
+++ src/text/text_style.jl
@@ -5,9 +5,17 @@ mutable struct TextStyle
 end
 
 function TextStyle(;
-    font=FreeTypeAbstraction.findfont("arial"),
+    font_name::String="",
+    font_path::String=joinpath(@__DIR__, "../../assets/fonts/FragmentMono-Regular.ttf"),
     size_px=16,
     color=Vec{4,Float32}(0.0, 0.0, 0.0, 1.0),
 )
+    if font_name != ""
+        font = FreeTypeAbstraction.findfont(font_name)
+    elseif font_path != ""
+        font = FreeTypeAbstraction.try_load(font_path)
+    else
+        error("Either font_name or font_path must be provided.")
+    end
     return TextStyle(font, size_px, color)
 end
